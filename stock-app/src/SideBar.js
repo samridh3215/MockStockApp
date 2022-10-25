3143e23f2998f664f67442f66e47db8fa71158a4
Samridh Anand <samridh@pop-os.localdomain>
2022-10-25 22:51:25 +0530

    solved the sidebar issue where it did not render

--- stock-app/src/SideBar.js
+++ stock-app/src/SideBar.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+const logopath = "media/icons8-w-67.png"
 function SideBar(){
      return(
           <div className="btn-group-vertical" role="group" aria-label="Basic example">
-               <img className="sidebar-icon" id="homebutton-icon" src="media/icons8-w-67.png" alt="icon"></img>
+               <img className="sidebar-icon" id="homebutton-icon" src={logopath} alt="icon"></img>
                <button type="button" className="btn btn-secondary">Profile</button>
                <br/>
                <button type="button" className="btn btn-secondary">Wallet</button>
