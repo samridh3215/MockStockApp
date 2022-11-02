c1018225661407d1e15066383d7c0069a98ac3be
samridh <samridhanand.2003@gmail.com>
2022-11-02 09:22:09 +0530

    added url for gainers and losers

--- stock-app/src/responsesFromAPI.js
+++ stock-app/src/responsesFromAPI.js
@@ -1,5 +1,6 @@
 const urlForTickers = "https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=6c26a26ee21c72ea8d84650fbefb2135"
-
+const urlForLosers = "https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=6c26a26ee21c72ea8d84650fbefb2135"
+const urlForGainers = "https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=6c26a26ee21c72ea8d84650fbefb2135"
 
 function getSymbols() {
     var a =[];
